7c4fc0055a88f077fae1ef6a5fe9abbe0fa3567f
trung-nguyen <71800077+trung-nguyen@users.noreply.github.com>
2020-09-24 10:21:23 +0700

    Add files via upload

--- App.js
+++ App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React trungnn
+          Learn React from iMac
         </a>
       </header>
     </div>

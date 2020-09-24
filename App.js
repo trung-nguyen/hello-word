f562cd7d5460531e77bef0de03572bacf3251ed2
trung-nguyen <nguyenngoctrung666@gmail.com>
2020-09-24 12:55:08 +0700

    doi

--- App.js
+++ App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          React Native
+          React Native trungnguyen
         </a>
       </header>
     </div>
